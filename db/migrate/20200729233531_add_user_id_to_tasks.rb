dec7af6015c5f829c627278b067fe1d1a02bab2e
miily8310s <rillakerika@gmail.com>
2020-07-30 08:37:41 +0900

    add user_to_tasks

---
+++ db/migrate/20200729233531_add_user_id_to_tasks.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddUserIdToTasks < ActiveRecord::Migration[6.0]
+  def up
+    execute 'DELETE FROM tasks'
+    add_reference :tasks, :user, null: false, index: true
+  end
+
+  def down
+    remove_reference :tasks, :user, index: true
+  end
+end
